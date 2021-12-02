066d667168856c58cb22389be7b80c76bc3052b9
Piotr Lejman <immoxyz@gmail.com>
2021-12-02 19:59:19 +0100

    adding menu scene

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var input = gameObject.GetComponent<InputField>();
+        input.onEndEdit.AddListener(SubmitName);
+    }
+    private void SubmitName(string arg0)
+    {
+        Debug.Log(arg0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//Helping to order UI handler to show on end
+[DefaultExecutionOrder(1000)]
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public static DataManager Instance;
+
+    /*[SerializeField] private InputField input;
+    public string playerName;
+
+
+    private void Start()
+    {
+        input = gameObject. //.GetComponentInChildren<InputField>();
+        var se = new InputField.SubmitEvent();
+        se.AddListener(SubmitName);
+        input.onEndEdit = se;
+    }
+
+    private void SubmitName(string arg0)
+    {
+        Debug.Log(arg0);
+        playerName = arg0;
+    }*/
+
+    public void StartBttnHandler()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitBttnHandler()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void SetPlayerName(string name)
+    {
+        DataManager.Instance.playerName = name;
+    }
+
+}
